ad334363267e26d917512154f6cec62540df5770
Laura <laurao148@gmail.com>
2018-04-13 03:04:21 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,7 +5,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.string :theme
-      t.price :float
+      t.float :price
       t.boolean :family_friendly
       t.text :opening_date
       t.text :closing_date
