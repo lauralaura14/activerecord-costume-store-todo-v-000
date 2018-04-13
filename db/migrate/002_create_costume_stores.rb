c9ab0b48f8b3a12b4373eb2c7dce50ba89be7817
Laura <laurao148@gmail.com>
2018-04-13 02:58:30 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,5 +2,12 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores
+      t.string :name
+      t.text :location
+      t.integer :costume_inventory
+      t.integer :employee_count
+      t.boolean :still_in_business
+      t.text :opening_time
+    end
   end
 end
