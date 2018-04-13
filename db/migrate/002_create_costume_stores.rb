5414a6a01818eccc556ff8d9cb14d1664f4aaa28
Laura <laurao148@gmail.com>
2018-04-13 03:18:32 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,7 +8,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.text :opening_time
-      t.datetime :closing_time
+      t.text :closing_time
     end
   end
 end
